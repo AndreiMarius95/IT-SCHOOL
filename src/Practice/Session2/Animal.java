5208d310c47b2611a006bbd234c8ead44f129e47
AndreiMarius95 <143540605+AndreiMarius95@users.noreply.github.com>
2023-09-14 20:38:01 +0300

    Firs Commit

--- src/Practice/Session2/Animal.java
+++ src/Practice/Session2/Animal.java
@@ -3,7 +3,7 @@ package Practice.Session2;
 public class Animal {
     public static void main(String[] args) {
         System.out.println("Hello Andrei");
-
+//
         //this is the homework
     }
 }
