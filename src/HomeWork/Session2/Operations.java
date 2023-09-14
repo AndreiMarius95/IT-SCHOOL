b6876ab9c9efb25029b97d9195afcbc738539e03
AndreiMarius95 <143540605+AndreiMarius95@users.noreply.github.com>
2023-09-14 20:52:39 +0300

    Firs Commit

---
+++ src/HomeWork/Session2/Operations.java
@@ -0,0 +1,18 @@
+package HomeWork.Session2;
+
+public class Operations {
+
+        public static void main(String[] args) {
+            int num1 = 11;
+            int num2 = 5;
+
+            System.out.println("Addition: " + (num1 + num2));
+            System.out.println("Subtraction: " + (num1 - num2));
+            System.out.println("Multiplication: " + (num1 * num2));
+            System.out.println("Division: " + (num1 / num2));
+            System.out.println("Modulus: " + (num1 % num2));
+
+        }
+    }
+
+
---
+++ src/HomeWork/Session2/SimpleInputOutput.java
@@ -0,0 +1,20 @@
+package HomeWork.Session2;
+
+import java.util.Scanner;
+
+public class SimpleInputOutput {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Numar1 :");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Numar2 :");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+
+        System.out.println("Result :" + sum);
+    }
+}
