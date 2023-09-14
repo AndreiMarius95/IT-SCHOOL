840c6e91cde3a234c5074f05a128dbee460b78a2
AndreiMarius95 <143540605+AndreiMarius95@users.noreply.github.com>
2023-09-14 20:35:58 +0300

    Firs Commit

---
+++ src/Practice/Session3/RelationalOperators.java
@@ -0,0 +1,16 @@
+package Practice.Session3;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 30;
+
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber != secondNumber);
+        System.out.println(firstNumber > secondNumber);
+        System.out.println(firstNumber < secondNumber);
+        System.out.println(firstNumber >= secondNumber);
+        System.out.println(firstNumber <= secondNumber);
+    }
+
+}
---
+++ src/Practice/Session3/UnaryOperators.java
@@ -0,0 +1,13 @@
+package Practice.Session3;
+
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+        int firstNumber = +10;
+        int secondNumber = -20;
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+    }
+}
+
