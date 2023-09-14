a8e904e91758c0a2a90e652e37f0f49178f79c82
AndreiMarius95 <143540605+AndreiMarius95@users.noreply.github.com>
2023-09-14 22:24:12 +0300

    Firs Commit

---
+++ src/HomeWork/Session4/Challenges2.java
@@ -0,0 +1,14 @@
+package HomeWork.Session4;
+
+public class Challenges2 {
+    public static void main(String[] args) {
+
+        int firstNumber = 5;
+        int secondNumber = 7;
+        System.out.println(firstNumber == secondNumber);
+
+        String firstString = "ITSCHOOL";
+        String secondString = "ITSCHOOL";
+        System.out.println(firstString.equals(secondString));
+    }
+}
---
+++ src/HomeWork/Session4/Challenges4.java
@@ -0,0 +1,20 @@
+package HomeWork.Session4;
+
+import java.util.Scanner;
+
+public class Challenges4 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Enter a height value(cm): ");
+            int heightOne = scanner.nextInt();
+
+            System.out.println("Enter another height value(cm): ");
+            int heightTwo = scanner.nextInt();
+
+            String result = (heightOne > heightTwo) ? "The first height value is greater than the second one." : "The second height value is greater than the first one.";
+            System.out.println(result);
+        }
+    }
+
