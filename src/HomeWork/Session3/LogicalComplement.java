c3c01589cc0a8592658a11dd24c5551a3a78ad40
AndreiMarius95 <143540605+AndreiMarius95@users.noreply.github.com>
2023-09-14 21:06:54 +0300

    Firs Commit

---
+++ src/HomeWork/Session3/LogicalComplement.java
@@ -0,0 +1,17 @@
+package HomeWork.Session3;
+
+public class LogicalComplement {
+    public static void main(String[] args) {
+
+        System.out.print("82");
+        int num = 10;
+
+        boolean pozitive = (num < 0);
+
+        if (pozitive)
+            System.out.println(num + " number pozitive");
+
+        System.out.println(num + " number negegative.");
+
+    }
+}
