176bf5c30269afb2035b8dd2bcd316d831e67996
AndreiMarius95 <143540605+AndreiMarius95@users.noreply.github.com>
2023-11-01 20:23:58 +0200

    Session 18

--- src/practice/session17/Product.java
+++ src/practice/session17/Product.java
@@ -1,3 +1,5 @@
+package practice.session17;
+
 public class Product {
 
     private String name;
--- src/practice/session17/StreamsPractice.java
+++ src/practice/session17/StreamsPractice.java
@@ -1,5 +1,7 @@
 package practice.session17;
 
+import practice.session8.shoppingcart.Product;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -8,27 +10,25 @@ import java.util.List;
 public class StreamsPractice {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         List<String> strings = Arrays.asList("apple", "banana", "peach", "watermelon");
-        List<session17.Product> products = Arrays.asList(
-                new Product("Phone", 999.9987),
-                new Product("Laptop", 2499.9987),
-                new Product("Gadged", 450.25));
+        List<Product> products = Arrays.asList(
+                new Product("Phone" + 599.9959),
+                new Product("Gadged" + 9450.99),
+                new Product("Laptop" + 2499.99));
         List<List<Integer>> listOfLists = Arrays.asList(
-                Arrays.asList(1, 2, 3, 4, 5),
-                Arrays.asList(2, 3, 4, 5, 6),
-                Arrays.asList(3, 4, 5, 6, 7));
+                Arrays.asList(1, 2, 3, 4), Arrays.asList(4, 5, 6, 7, 8, 9), Arrays.asList(9, 10));
+//        printEvenNumbers(integerList);
 
-//        printEvenNumbers(numbers);
-//        printEvenNumbersWithStream(numbers);
-//        printStringsByLengthBiggerThan(strings, 5);
+//        printEveneNumbersWithStream(integerList);
+//        printStringsByLength(strings, 6);
 //        printStringsLength(strings);
-//        printProductPrices(products);
+//        printProductsPrice(products);
 //        printProductIncreasedPrices(products);
 //        printFlatMap(listOfLists);
-//        printSumOfNumbers(numbers);
+//        printSumOfNumbers(integerList);
 //        printSortedProductsByPrice(products);
-//        printProcessedNumbers(numbers);
+//        printProcessedNumbers(integerList);
         printAnyMatchFruit(strings);
     }
 
@@ -36,14 +36,14 @@ public class StreamsPractice {
         boolean anyMatch = strings.stream()
                 .filter(fruit -> fruit.length() > 5)
                 .anyMatch(fruit -> fruit.startsWith("a"));
-        System.out.println("Is there any fruit with length > 5 and that starts with 'a'? " + anyMatch);
+        System.out.println("Is there any fruit with length > 5 and that starts with 'a': " + anyMatch);
     }
 
     private static void printProcessedNumbers(List<Integer> integerList) {
         integerList.stream()
-                .filter(number -> number % 2 == 0)
-                .map(number -> number * number)
-                .filter(number -> number > 20)
+                .filter(num -> num % 2 == 0)
+                .map(num -> num * num)
+                .filter(num -> num > 20)
                 .forEach(System.out::println);
     }
 
@@ -51,13 +51,13 @@ public class StreamsPractice {
         List<Product> sortedProducts = productList.stream()
                 .sorted(Comparator.comparingDouble(Product::getPrice))
                 .toList();
-        sortedProducts.forEach(product -> System.out.println(product.getName() + " " + product.getPrice()));
+        sortedProducts.forEach(product -> System.out.println(product.getName() + " : " + product.getPrice()));
     }
 
-    private static void printSumOfNumbers(List<Integer> numbers) {
-        int sum = numbers.stream()
+    private static void printSumOfNumbers(List<Integer> integerList) {
+        int sum = integerList.stream()
                 .reduce(0, Integer::sum);
-        System.out.println("Sum of numbers: " + sum);
+        System.out.println("Sum of numbers using reduce: " + sum);
     }
 
     private static void printFlatMap(List<List<Integer>> listOfLists) {
@@ -68,57 +68,55 @@ public class StreamsPractice {
     }
 
     private static void printProductIncreasedPrices(List<Product> productList) {
-        List<String> formattedPrices = productList.stream()
+        List<String> formatedPrices = productList.stream()
                 .map(product -> product.getPrice() * 1.10)
                 .map(newPrice -> "$" + String.format("%.2f", newPrice))
                 .toList();
-        System.out.println("Increased prices: " + formattedPrices);
+        System.out.println("Increased prices: " + formatedPrices);
     }
 
-    private static void printProductPrices(List<Product> productList) {
+    private static void printProductsPrice(List<Product> productList) {
         productList.stream()
                 .map(Product::getPrice)
                 .forEach(System.out::println);
     }
 
-    private static void printStringsLength(List<String> stringList) {
-        List<Integer> resultList = stringList.stream()
+    private static void printStringsLength(List<String> inputList) {
+        List<Integer> list = inputList.stream()
                 .map(String::length)
                 .toList();
-        System.out.println("The length of the strings: " + resultList);
+        System.out.println("String lengths: " + list);
     }
 
-    private static void printStringsByLengthBiggerThan(List<String> inputList, int minLength) {
-        List<String> filteredStrings = new ArrayList<>();
-        for (String element : inputList) {
-            if (element.length() > minLength) {
-                filteredStrings.add(element);
+    private static void printStringsByLength(List<String> list, int minLength) {
+        List<String> filteredList = new ArrayList<>();
+        for (String str : list) {
+            if (str.length() >= minLength) {
+                filteredList.add(str);
             }
         }
-        System.out.println("Traditional approach: " + filteredStrings);
+        System.out.println("Traditonal approach: " + filteredList);
 
-        List<String> filteredStringsLambda = inputList.stream()
-                .filter(element -> element.length() > minLength)
+        List<String> filterWithLambda = filteredList.stream()
+                .filter(str -> str.length() >= minLength)
                 .toList();
-        System.out.println("Stream approach: " + filteredStringsLambda);
+        System.out.println("Stream approach: " + filterWithLambda);
     }
 
-    private static void printEvenNumbersWithStream(List<Integer> numbers) {
-        // stream approach
-        List<Integer> evenNumbersStream = numbers.stream()
+    private static void printEveneNumbersWithStream(List<Integer> integerList) {
+        List<Integer> evenNumsStream = integerList.stream()
                 .filter(number -> number % 2 == 0)
                 .toList();
-        System.out.println("Stream approach: " + evenNumbersStream);
+        System.out.println("Stream approach: " + evenNumsStream);
     }
 
-    private static void printEvenNumbers(List<Integer> numbers) {
-        // traditional approach
-        List<Integer> evenNumbers = new ArrayList<>();
-        for (Integer number : numbers) {
-            if (number % 2 == 0) {
-                evenNumbers.add(number);
+    private static void printEvenNumbers(List<Integer> integerList) {
+        List<Integer> evenNums1 = new ArrayList<>();
+        for (int num : integerList) {
+            if (num % 2 == 0) {
+                evenNums1.add(num);
             }
         }
-        System.out.println("Traditional approach: " + evenNumbers);
+        System.out.println("Traditional way: " + evenNums1);
     }
 }
